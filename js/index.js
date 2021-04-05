7f415f59dcbac5679489ee1ed8e133708d66f81c
Bobby Rong <rjl931189261@126.com>
2021-04-05 21:17:38 +0800

    major reorganize

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const TodoList = {
+    data() {
+      return {
+        groceryList: [
+          { id: 0, text: 'Vegetables' },
+          { id: 1, text: 'Cheese' },
+          { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+      }
+    }
+  }
+  
