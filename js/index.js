7cbf43b569f8665e720c45b19206e4a819341074
Bobby Rong <rjl931189261@126.com>
2021-04-17 17:52:32 +0800

    separate side link to a single json file

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-function loadJSON(callback) {
+var supported_os_file = '/fancyindex/data/supported_os.json';
+var side_link_file = '/fancyindex/data/side_link.json';
+
+function loadJSON(json_url, callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '/fancyindex/json/supported_os.json', true);
+    xobj.open('GET', json_url, true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             callback(xobj.responseText);
@@ -14,12 +17,16 @@ var app = Vue.createApp({
     data() {
         return {
             supported_os: [],
+            side_link: [],
         }
     },
     mounted() {
         var self = this;
-        loadJSON(function (response) {
+        loadJSON(supported_os_file, function (response) {
             self.supported_os = JSON.parse(response);
         });
+        loadJSON(side_link_file, function (response) {
+            self.side_link = JSON.parse(response);
+        });
     }
-}).mount('#os-list');
+}).mount('#app');
